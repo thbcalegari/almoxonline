ba5c962e83808e42487f71f585396ed3372c0e55
thbcalegari <thbcalegari@protonmail.com>
2022-09-17 01:05:58 -0300

    pagina inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>AlmoxOnLine - Controle de estoque</title>
+
+    <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>AlmoxOnLine</h1>
+                    <br/>
+                    <h3>Sistema de controle de estoque para almoxarifados.</h3>
+                    <a href="{{ route('dashboard') }}">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
